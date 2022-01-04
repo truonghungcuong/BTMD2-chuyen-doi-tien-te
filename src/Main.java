b31fde94c45e35e99ce14078032a60dfa49bb5a7
truonghungcuong <95598994+truonghungcuong@users.noreply.github.com>
2022-01-04 17:26:32 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double vnd=23000;
         double usd;
         Scanner scanner=new Scanner(System.in);
         System.out.println("Nhập số tiền (USD): ");
